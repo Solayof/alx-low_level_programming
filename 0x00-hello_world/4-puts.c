f9355341dd161b230ee478c2719319385933742d
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-07-13 14:00:27 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
- * The code print out string and return zero if run successfully
+ * main - Start point
+ * 
+ * Return:  zero if run successfully
  */
 
-int main(void) {
-	puts("Programming is like building a multilingual puzzle\n");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
