64920de5c96948858c1eae6432638e18709b9513
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-07-13 14:21:28 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - Starting point
+ *
+ *Return: zero if sucessfull
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
