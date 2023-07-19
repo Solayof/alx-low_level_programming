91f2c32bd2dff6367b11ad24b2cbfa44bd474935
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-07-18 18:12:24 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,50 @@
 #include "main.h"
 /**
- * times_table -prints tables
+ * 11-print_to_98 -prints tables
  *
  * Return: Always 0.
  */
-void times_table(void)
+void times_table(int n)
+{
+	if (n >= 98)
+	{
+		for (; n <= 98; n++)
+		{_putchar (',');
+                                _putchar (' ');
+			if (n < 10)
+			{
+				_putchar (n + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+
+			_putchar ((n / 10) + '0');
+			_putchar ((n % 10) + '0');
+			if (n != 98)
+			{
+				_putchar (',');
+                                _putchar (' ');
+			}
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n > 99)
+			{
+				_putchar ((n / 100) + '0');
+				_putchar ((n / 10) + '0');
+				_putchar ((n % 10) + '0');
+				_putchar (',');
+                                _putchar (' ');
+			}
+			_putchar ((n / 10) + '0');
+                        _putchar ((n % 10) + '0');
+			if (n != 98)
+                        {
+                                _putchar (',');                                   _putchar (' ');
+                        }
+		}
+	}
+}
