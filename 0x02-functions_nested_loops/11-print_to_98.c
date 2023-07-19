b52498c9a9684d45b96f03f44797c77df701bcb4
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-07-18 18:35:33 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,10 +13,10 @@ void print_to_98(int n)
 			if (n < -9)
 			{
 				_putchar ((n / 10) + '0');
-				-putchar ((n % 10) + '0');
+				_putchar ((n % 10) + '0');
 				_putchar (',');
                                 _putchar (' ');                           }
-			else if (n = -9 && n < 10)
+			else if ((n = -9) && (n < 10))
 			{
 				_putchar (n + '0');
 				_putchar (',');
