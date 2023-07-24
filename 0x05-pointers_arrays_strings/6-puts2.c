f08118eba25c420e2247471e8135308f7c6013dd
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-07-24 12:39:36 -0700

    puts2 created

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++
+		str++
+	}
+	int i;
+	for (i = 0; str[i] != '\0' && i < len; i += 2)
+		_putchar(str[i]);
+
+	_putxhar('\n');
+
+}
