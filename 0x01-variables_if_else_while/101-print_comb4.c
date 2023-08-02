f8cd959d1bf92c54a8c98f510c46cf3bbd9ec03e
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-01 17:18:26 -0700

    if and only

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@
 int main(void)
 {
 	int i, j, k, l;
-	
+
 	for (i = 0; i < 1000; i++)
 	{
 		j = i / 100; /* hundreds */
@@ -26,7 +26,7 @@ int main(void)
 		{
 			putchar(j + '0');
 			putchar(k + '0');
-		       	putchar(l + '0');
+			putchar(l + '0');
 		if (i < 700)
 		{
 			putchar(44);
