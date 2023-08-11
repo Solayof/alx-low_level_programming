8d80be57ec5efdb27a11ad652c59ac584c57f8cf
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-10 18:06:53 -0700

    tmp

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,126 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * _steprintf - print string only follow by new line
- *  @s: string to print.
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
  */
-
-_strprintf(char *s)
+void _is_zero(char *argv[])
 {
-       	int i = 0;
-	while (s[i] != '\0')
+	int i, isn1 = 1, isn2 = 1;
+
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
 	{
-		_putchar(s[i]);
-		i++
+		printf("0\n");
+		exit(0);
 	}
-	
-	_putchar('\0');
 }
 
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
 
 /**
- * isnum - determines if the string arg is a num
- * convert the string arg to number if.
- * @s: string to test and convert to number
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
  */
-
-
-int isnum(char *s)
+int _checknum(char *argv[], int n)
 {
-       	int i;
+	int ln;
 
-	char *err = "Error";
-	for (i = 0; s(i) != '\0'; i++)
-	{
-		if (s[i] < '0' || s[i] > '9')
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
-			_strprintf(err);
+			printf("Error\n");
 			exit(98);
 		}
-	}
 
-	return (atoi(s));
-}
- 
-
-/**
- * numprintf - print number to standard output
- * @num: number to print.
- */
-
-numprintf(int num)
-{
-	int x = num;
-
-	if (x > 9)
-	{
-		numprintf(x / 10);
-	}
-	_putchar(x % 10 + '0');
+	return (ln);
 }
 
 /**
- * main - main entry.
- * @numc: counter: numbers of arguments.
- * @numarr: array holding the numbers.
- * Return: returns 0 if successful.
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
  */
-
-int main(int numc, char **numarr)
+int main(int argc, char *argv[])
 {
-	char *err = "Error";
-	int num1, num2, mul;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
-	if (numc != 3)
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-
-		_strprintf(err);
-		exit(98);
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-
-	num1 = numarr[1];
-	num2 = numarr[2];
-
-	mul = num1 * num2;
-
-	numprintf(mul);
-	_putchar('\0')
+	printf("%s\n", nout);
+	return (0);
 }
