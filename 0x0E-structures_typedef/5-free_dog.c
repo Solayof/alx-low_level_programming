0e4dcdf2941736fd6e01794a13fc10bc5178a30c
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-14 20:48:50 +0100

    struct visited

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,20 @@
-nclude "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
+
 /**
- * free_dog - frees memory of structure dogs
- * @d: pointer of structure
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * free_dog - free instance of struct dog
+ * @d: instance to be freed
  */
+
 void free_dog(dog_t *d)
 {
-	if (d != 0)
-	{
-		free(*d).name);
-		free(*d).age);
-		free(*d).owner);
-		free(*d);
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name) /* free members of struct that exist */
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
 }
