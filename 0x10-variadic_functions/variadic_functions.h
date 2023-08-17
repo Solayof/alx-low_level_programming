9a5dfa0dadb724a2953e7689fe7890c056b26699
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-17 15:47:59 +0100

    struct define1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -8,7 +9,7 @@ void print_all(const char * const format, ...)
 ;
 typedef struct 
 {
-	char letter;
+	char *letter;
 	void (*func)(va_list valist);
 } datatype;
 #endif
