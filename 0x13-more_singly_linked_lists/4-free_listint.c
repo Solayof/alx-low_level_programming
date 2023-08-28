389af928b35cb94a9cb7c65341881c9fcacb66fb
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-28 23:28:48 +0100

    updated

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a list
+ * @head: head of the list to be free
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *free_node;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free_node = head;
+		head = head->next;
+		free(free_node);
+	}
+}	
