051ee31d75a19569d5636069725f5f1f8d24ff6b
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-29 00:53:39 +0100

    updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
-
+/**
+ * get_nodeint_at_index - get a node with a given index.
+ * @head: head of the node.
+ * @index: indeeex  of the node to get.
+ * Return: returns pointer to the given index.
+ */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
