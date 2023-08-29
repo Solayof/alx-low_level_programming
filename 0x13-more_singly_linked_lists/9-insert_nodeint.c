6fed614e135abf12cee11acef879727bdaf211aa
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-29 13:10:56 +0100

    updated

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *h, *tmp;
+	int i = 0;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	h = *head;
+
+	while ((i < idx) && (h != NULL))
+	{
+		tmp = h;
+		h = h->next;
+		i++;
+	}
+
+	if (idx = 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	if (n == idx)
+	{
+		new->next = h;
+		tmp = new;
+		return (tmp);
+
+	}
+
+	free(new);
+	return (NULL);
+}
