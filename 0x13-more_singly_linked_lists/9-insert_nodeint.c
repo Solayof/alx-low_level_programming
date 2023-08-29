87c5ce00fc6526ccd2df6c5b97e02530bce3beaf
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-08-29 13:13:29 +0100

    updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	if (n == idx)
+	if (i == idx)
 	{
 		new->next = h;
 		tmp = new;
