b10559b9acd3339dd00a7b045843e692336bdaa0
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-09-04 21:44:36 +0100

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i = 0;
-	size_t num_w;
+	int num_w;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (*text_content != NULL)
+	while (*text_content != '\0')
 		i++;
 
 	if (i > 0)
