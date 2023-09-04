e0eee59948f917776eaa65843fa1e2f7b394a9cb
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-09-04 19:05:36 +0100

    auto git commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * read_textfile - read file and print it sto stdout.
+ * @filename: file to read.
+ * @letters: charater bytes to read.
+ * Return: returns number of characters read or zero if fail.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t num_read, num_w;
+	int fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters * (sizeof(char)));
+
+	if (buffer == NULL)
+		return (0);
+
+	num_read = read(fd, buffer, letters);
+
+	if (num_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	num_w = write(STDOUT_FILENO, buffer, num_Read);
+
+	if (num_w == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (num_read);
+
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - function that file.
+ * @filename: name of file to create.
+ * @text_content: content of the file.
+ * Return: 1 if successfull and -1 if failed.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+	size_t num_w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content != NULL)
+		i++;
+
+	if (i > 0)
+	{
+		num_w = write(fd, text_content, i);
+		if (num_w == -1 || num_w != i)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
