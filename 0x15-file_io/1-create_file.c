268a8a074611d8c620ff09c0fc4488a3da8bf242
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-09-05 22:34:44 +0100

    upf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 
-	while (*text_cintent != '\0')
+	while (*(text_cintent +i)  != '\0')
 		i++;
 
 	num_w = write(fd, text_content, i);
