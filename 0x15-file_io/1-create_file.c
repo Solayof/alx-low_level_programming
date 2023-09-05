1c8162230c05713a90340cd992af4d7b141e98b3
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-09-05 22:25:15 +0100

    ce pu

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 
 	num_w = write(fd, text_content, i);
-	if (num_w == -1 || num_w != _i)
+	if (num_w == -1 || num_w != i)
 	{
 		close(fd);
 		return (-1);
