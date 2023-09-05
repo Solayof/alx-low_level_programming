4978e2aa99f77b40b718ce2230902831293ee11f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-09-05 04:29:28 +0100

    upf

--- 0x15-file_io/tt.c
+++ 0x15-file_io/tt.c
@@ -9,34 +9,34 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	size_t num_read, num_w;
 	int fd;
-	ssize_t num_read, num_w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
+	buffer = malloc(letters * (sizeof(char)));
 
-	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
 	num_read = read(fd, buffer, letters);
+
 	if (num_read == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
+	num_w = write(STDOUT_FILENO, buffer, num_Read);
 
-
-	num_w = write(STDOUT_FILENO, buffer, n_read);
-	if (n_w == -1)
+	if (num_w == -1)
 	{
 		free(buffer);
 		close(fd);
