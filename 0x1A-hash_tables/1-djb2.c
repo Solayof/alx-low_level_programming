6c23801992834f9ae11e3b79a01dc80779af233d
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-11-30 20:45:06 +0100

    unsigned long int hash_djb2(const unsigned char *str)

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash.h"
+
+/**
+ * hash_djb2 - generate random number from the given string
+ * @str: string
+ * Return: int
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int num = 9973;
+	int i;
+
+	while (str)
+	{
+		i = *str;
+		num = ((num << 3) + i) + num << 2;
+	}
+	return (num);
+}
